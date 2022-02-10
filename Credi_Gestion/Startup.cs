7bef0c6cf4a83053ed72167ab68dde8432c4f612
jaime <jgomezm1991@gmail.com>
2022-02-10 16:56:10 -0600

    pendiente la la clece

--- Credi_Gestion/Startup.cs
+++ Credi_Gestion/Startup.cs
@@ -23,6 +23,9 @@ namespace Credi_Gestion
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Conmfiguración de cadena de conexion
+            services.addDbContext<ApplicationDbContext>(Options=>Options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllersWithViews();
         }
 
