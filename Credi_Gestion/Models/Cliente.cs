d80a95b21b2ef5adef2d3658cd31bc0c36fa2b8e
jaime.gomez <jgomezm1991@gmail.com>
2022-02-11 06:47:54 -0600

    Agregar clace ApplicationDbcontex

--- Credi_Gestion/Models/Cliente.cs
+++ Credi_Gestion/Models/Cliente.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
+
 namespace Credi_Gestion.Models
 {
     public class Cliente
@@ -11,37 +12,37 @@ namespace Credi_Gestion.Models
         [Key]
         public int Id { get; set; }
 
-        [Required=(ErrorMessage="El nombre del cliente es requerido")]
+        [Required(ErrorMessage="El nombre del cliente es requerido")]
         public string NombreCliente { get; set; }
 
-        [Required=(ErrorMessage="El número de cedula es requerido")]
+        [Required(ErrorMessage="El número de cedula es requerido")]
         [StringLength(100,ErrorMessage ="La {0} debe tener al menos {2} y máximo {1} caracteres",MinimumLength =3)]
         [Display(Name="Cédula")]
         public string Cedula { get; set; }
 
-        [Required=(ErrorMessage="La dirección  es requerida")]
+        [Required(ErrorMessage="La dirección  es requerida")]
         [StringLength(100,ErrorMessage ="La {0} debe tener al menos {2} y máximo {1} caracteres",MinimumLength =3)]
         [Display(Name="Cédula")]
         public string Direccion { get; set; }
 
-        [Required=(ErrorMessage="Debe ingresar el número de teléfono")]
+        [Required(ErrorMessage="Debe ingresar el número de teléfono")]
         [Display(Name ="Teléfono")]
         public string Telefono { get; set; }
 
-        [Required=(ErrorMessage="Debe ingresar el monto del préstamo")]
-        [Display=(Name="Préstamo")]
+        [Required(ErrorMessage="Debe ingresar el monto del préstamo")]
+        [Display(Name="Préstamo")]
         public decimal Prestamo { get; set; }
 
-        [Required=(ErrorMessage="Debe ingresar el saldo")]
+        [Required(ErrorMessage="Debe ingresar el saldo")]
         public decimal Saldo { get; set; }
 
-        [Required=(ErrorMessage="Debe seleccionar la fecha")]
+        [Required(ErrorMessage="Debe seleccionar la fecha")]
             // guardar en la base de datos con fecha y hora
-        [DataType=(DataType.Date)]
-        [Display=(Name="Fecha del registro")]
+        [DataType(DataType.Date)]
+        [Display(Name="Fecha del registro")]
         public DateTime FechaReg { get; set; }
 
-        [Display=(Name:"Usuario que registró")]
+        [Display(Name="Usuario que registró")]
         public string UsuarioRe { get; set; }
         
 
