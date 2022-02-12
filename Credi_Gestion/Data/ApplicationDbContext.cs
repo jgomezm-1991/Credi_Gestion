cb896600a7b16e85ccb4e8a3cb57e579e9d5b8eb
jaime.gomez <jgomezm1991@gmail.com>
2022-02-12 07:57:32 -0600

    Analizar errores

--- Credi_Gestion/Data/ApplicationDbContext.cs
+++ Credi_Gestion/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Credi_Gestion.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,9 +9,11 @@ namespace Credi_Gestion.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext()
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>Options):base(Options)
         {
                     
         }
+
+        public DbSet<Cliente> Cliente  { get; set; } 
     }
 }
--- Credi_Gestion/Startup.cs
+++ Credi_Gestion/Startup.cs
@@ -1,6 +1,8 @@
+using Credi_Gestion.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,8 +25,8 @@ namespace Credi_Gestion
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //Conmfiguración de cadena de conexion
-            services.addDbContext<ApplicationDbContext>(Options=>Options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //Conmfiguración de cadena de conexion  
+            services.AddDbContext<ApplicationDbContext>(Options=>Options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllersWithViews();
         }
